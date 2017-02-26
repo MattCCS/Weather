aab543097be452983962d99c7b920a625fe2cb0f
Matthew Cotton <matt@thecottons.com>
2017-02-26 10:48:26 -0500

    Add support for language choice

--- readme.md
+++ readme.md
@@ -16,4 +16,6 @@ How to Use
 ----------
 Run `weather` to get the weather in your zip code (checks ipinfo.io for your zip code)
 
-Run `weather <location>` to get the weather at the given location (can be zip code/city/whatever wttr.in likes)
+Run `weather <location>` to get the weather at the given location (can be zip code/city/IATA code/whatever wttr.in likes)
+
+Use the `-l / --lang` flag to pass a language code (e.g., "en", "it"). Only changes the report, and must be supported by wttr.in. Default is "en".
--- weather.py
+++ weather.py
@@ -11,7 +11,9 @@ import urllib.request
 
 
 URL_ZIPCODE = "http://ipinfo.io"
-URL_WEATHER_FORM = "http://wttr.in/{}"
+URL_WEATHER_FORM = "http://wttr.in/{}?lang={}"
+
+DEFAULT_LANG = "en"
 
 CURL_HEADER = {
     'User-Agent': 'curl/7.43.0',
@@ -30,10 +32,7 @@ def get_zipcode_by_ip():
     return data['postal']
 
 
-def get_location():
-    args = parse_args()
-    location = args.location
-
+def guarantee_location(location):
     # default behavior is to find zip code
     if not location:
         print("[ ] No location provided, finding zip code...")
@@ -52,10 +51,12 @@ def is_valid_zipcode(zipcode):
     return zipcode.isdigit() and len(zipcode) == 5
 
 
-def get_weather_by_location(location):
-    print("[ ] Getting weather for {}...".format(location))
+def get_weather_by_location(location, language):
+    url = URL_WEATHER_FORM.format(location, language)
+    print("[ ] Getting weather for {}... ({})".format(location, url))
+
     req = urllib.request.Request(
-        URL_WEATHER_FORM.format(location),
+        url,
         data=None,
         headers=CURL_HEADER,
     )
@@ -66,15 +67,17 @@ def get_weather_by_location(location):
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Prints a weather report for the given location.  Uses your zip code if no location provided."
+        description="Prints a weather report for the given location. Uses your zip code if no location provided."
     )
     parser.add_argument("location", help="Zip code or city (optional)", nargs="?")
+    parser.add_argument("-l", "--lang", help="The weather report langauge", default=DEFAULT_LANG)
     return parser.parse_args()
 
 
 def main():
-    location = get_location()
-    print(get_weather_by_location(location))
+    args = parse_args()
+    location = guarantee_location(args.location)
+    print(get_weather_by_location(location, args.lang))
 
 
 if __name__ == '__main__':
